424b1ff077208f3f2e31bc0c82e180320ac582d5
beginerbeginer190105 <miyabi6125@gmail.com>
2019-02-19 20:00:37 +0900

    edit app/controllers/groups_controller.rb
    追加  def set_group

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -29,7 +29,13 @@ class GroupsController < ApplicationController
   end
 
   private
+
   def group_params
     params.require(:group).permit(:name, { :user_ids => [] })
   end
+
+  def set_group
+    @group = Group.find(params[:id])
+  end
+
 end
