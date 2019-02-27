1753c5142c6e129238f4636811466d861eb10e9e
beginerbeginer190105 <miyabi6125@gmail.com>
2019-02-27 17:47:59 +0900

    edit app/assets/javascripts/users.js
    add $(document).on('turbolinks:load,~

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -1,4 +1,5 @@
-$(function() {
+$(document).on('turbolinks:load', function() {
+
   function appendUser(user) {
      var html = `<div class="chat-group-user clearfix">
                    <p class="chat-group-user__name">${user.name}</p>
@@ -14,8 +15,11 @@ $(function() {
                 </div>`;
       return html;
   }
+
   $("#user-search-field").on("keyup", function() {
     var input = $("#user-search-field").val();
+     console.log(input)
+
     $.ajax({
       type: 'GET',
       url: '/users',
@@ -34,6 +38,7 @@ $(function() {
         alert('ユーザー検索に失敗しました');
     });
   });
+
   $(".chat-group-form").on('click', ".user-search-add", function() {
       var id = $(this).data('user-id');
       var name = $(this).data('user-name');
@@ -42,6 +47,7 @@ $(function() {
       var user = $(this).parent();
       user.remove();
   });
+
   $(".chat-group-form").on('click', ".user-search-remove", function() {
       var user = $(this).parent();
       user.remove();
