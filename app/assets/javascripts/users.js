49a0fe4913a5d70103858f690d4f75292d556baf
beginerbeginer190105 <miyabi6125@gmail.com>
2019-02-27 19:55:29 +0900

    edit app/assets/javascripts/users.js

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -18,7 +18,6 @@ $(document).on('turbolinks:load', function() {
 
   $("#user-search-field").on("keyup", function() {
     var input = $("#user-search-field").val();
-     console.log(input)
 
     $.ajax({
       type: 'GET',
